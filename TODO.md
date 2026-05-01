2e8bd887e011bb9d701859f86751762718bc64bc
Matthias <5011972+fasmat@users.noreply.github.com>
2026-05-01 19:56:16 +0000

    Adjust simulation to take runs independently instead of averaging years accross simulations

--- TODO.md
+++ TODO.md
@@ -5,7 +5,3 @@
   - The money is always taken out in a way to get closest to the target distribution of the portfolio. For example, if
     the target distribution is 70% stocks and 30% bonds, and the portfolio is currently at 80% stocks and 20% bonds,
     then the money will be taken out from the stocks first to get closer to the target distribution.
-- Median and quantiles of the drawdown are taken across each year instead of across the entire simulation, which might
-  make the results appear worse (but definitely less volatile) than they should be.
-  - Instead sort the simulations by total return (payouts + remaining portfolio value) and take the median and
-    quantiles across the simulations instead of across the years.
--- drawdown.R
+++ drawdown.R
@@ -4,9 +4,9 @@ simulate_drawdown <- function(
     initial_portfolio_value = 1000000,        # Initial portfolio value
     expected_return = 0.05,                   # Expected yearly return (e.g., 5%)
     drawdown_period = 30,                     # Number of years in the drawdown period
-    historical_data_file = "sp500.csv",       # Path to the historical returns data file
+    historical_data_file = "msci_world.csv",  # Path to the historical returns data file
     max_payout = default_payout,              # Maximum payout function (e.g., $50,000 per year)
-    num_simulations = 10000                   # Number of simulations to run
+    num_simulations = 100000                  # Number of simulations to run
 ) {
     # styler: on
     # Load historical returns data
@@ -55,7 +55,7 @@ simulate_drawdown <- function(
 
 # Example of a maximum payout function that adjusts for inflation at 3% per year
 default_payout <- function(year) {
-    starting_payout <- 65000
+    starting_payout <- 66000
     starting_payout * 1.03^(year - 1)
 }
 
@@ -69,43 +69,47 @@ annuity_rate <- function(remaining_years = 30, interest_rate = 0.05) {
 }
 
 plot_drawdown <- function(payouts) {
-    # Calculate the median 10% and 90% quantiles of the payouts for each year
-    payouts_median <- apply(payouts[, 1:(ncol(payouts) - 1)], 2, median)
-    payouts_q05 <- apply(payouts[, 1:(ncol(payouts) - 1)], 2, quantile, probs = 0.05)
-    payouts_q10 <- apply(payouts[, 1:(ncol(payouts) - 1)], 2, quantile, probs = 0.10)
-    payouts_q25 <- apply(payouts[, 1:(ncol(payouts) - 1)], 2, quantile, probs = 0.25)
+    # sort the payouts by their total sum to get the worst-case scenarios at the top
+    payouts <- payouts[order(rowSums(payouts)), ]
 
-    remaining_values <- payouts[, ncol(payouts)]
-    remaining_median <- median(remaining_values)
-    remaining_q05 <- quantile(remaining_values, probs = 0.05)
-    remaining_q10 <- quantile(remaining_values, probs = 0.10)
-    remaining_q25 <- quantile(remaining_values, probs = 0.25)
+    samples <- nrow(payouts)
+
+    # Calculate the median 10% and 90% quantiles of the payouts for each year
+    payouts_median <- payouts[samples * 0.5, ]
+    payouts_q05 <- payouts[samples * 0.05, ]
+    payouts_q10 <- payouts[samples * 0.10, ]
+    payouts_q25 <- payouts[samples * 0.25, ]
+    payouts_q75 <- payouts[samples * 0.75, ]
 
     # Create a plot of calculated payouts over time
     jpeg("drawdown_plot.jpg", width = 800, height = 600)
-    plot(seq_along(payouts_median), payouts_median,
+    plot(
+        seq_len(length(payouts_median) - 1),
+        payouts_median[-length(payouts_median)],
         type = "l", col = "blue",
         xlab = "Year", ylab = "Median Payout", main = "Median Payout Over Time",
-        ylim = c(0, max(payouts_median) * 1.1)
+        ylim = c(0, max(payouts_median[-length(payouts_median)]) * 1.1)
     )
-    lines(seq_along(payouts_q05), payouts_q05, col = "darkred", lty = 2)
-    lines(seq_along(payouts_q10), payouts_q10, col = "red", lty = 2)
-    lines(seq_along(payouts_q25), payouts_q25, col = "orange", lty = 2)
+    lines(seq_len(length(payouts_q05) - 1), payouts_q05[-length(payouts_q05)], col = "darkred", lty = 2)
+    lines(seq_len(length(payouts_q10) - 1), payouts_q10[-length(payouts_q10)], col = "red", lty = 2)
+    lines(seq_len(length(payouts_q25) - 1), payouts_q25[-length(payouts_q25)], col = "orange", lty = 2)
+    lines(seq_len(length(payouts_q75) - 1), payouts_q75[-length(payouts_q75)], col = "green", lty = 2)
     legend("topright",
-        legend = c("Median", "5th Percentile", "10th Percentile", "25th Percentile"),
-        col = c("blue", "darkred", "red", "orange"), lty = c(1, 2, 2, 2)
+        legend = c("Median", "5th Percentile", "10th Percentile", "25th Percentile", "75th Percentile"),
+        col = c("blue", "darkred", "red", "orange", "green"), lty = c(1, 2, 2, 2, 2)
     )
     grid()
 
     # Add info about remaining portfolio value at the end of the drawdown period
     text(
-        x = length(payouts_median) * 0.2, y = max(payouts_median) * 0.9,
+        x = length(payouts_median) * 0.2, y = max(payouts_median[-length(payouts_median)]) * 0.9,
         labels = paste0(
             "Remaining Portfolio Value at Year ", ncol(payouts) - 1, ":\n",
-            "Median: $", round(remaining_median, 2), "\n",
-            "5th Percentile: $", round(remaining_q05, 2), "\n",
-            "10th Percentile: $", round(remaining_q10, 2), "\n",
-            "25th Percentile: $", round(remaining_q25, 2)
+            "Median: $", round(payouts_median[length(payouts_median)], 2), "\n",
+            "5th Percentile: $", round(payouts_q05[length(payouts_q05)], 2), "\n",
+            "10th Percentile: $", round(payouts_q10[length(payouts_q10)], 2), "\n",
+            "25th Percentile: $", round(payouts_q25[length(payouts_q25)], 2), "\n",
+            "75th Percentile: $", round(payouts_q75[length(payouts_q75)], 2)
         ),
         cex = 0.8, col = "black"
     )
